150aa53e1a749b91a707406c5e6ae14ed77f20f6
duck <godqhr2256@gmail.com>
2022-03-28 22:30:07 +0900

    [frontend] postCard 제작~ing

--- frontend/components/PostCard.js
+++ frontend/components/PostCard.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Card, Popover, Button } from "antd";
+import {
+  EllipsisOutlined,
+  HeartOutlined,
+  MessageOutlined,
+  RetweetOutlined,
+} from "@ant-design/icons";
+import ButtonGroup from "antd/lib/button/button-group";
+import PostImages from "./PostImages";
 
-const PostCard = () => {
-  return <div>postCard</div>;
+const PostCard = ({ post }) => {
+  return (
+    <div>
+      <Card
+        cover={post.Images[0] && <PostImages />}
+        actions={[
+          <RetweetOutlined key="retweet" />,
+          <HeartOutlined key="heart" />,
+          <MessageOutlined key="comment" />,
+          <Popover
+            key="more"
+            content={
+              <ButtonGroup>
+                <Button>수정</Button>
+                <Button type="danger">삭제</Button>
+                <Button>신고</Button>
+              </ButtonGroup>
+            }
+          >
+            <EllipsisOutlined />
+          </Popover>,
+        ]}
+      ></Card>
+    </div>
+  );
 };
 
 export default PostCard;
---
+++ frontend/components/PostImages.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PostImages = () => {
+  null;
+};
+
+export default PostImages;
