388dc2fb6e8325d3c7a5f2daecc49c6a165b1c15
duck <godqhr2256@gmail.com>
2022-04-03 01:29:53 +0900

    [frontend] 댓글 기능 구현

--- frontend/components/CommentForm.js
+++ frontend/components/CommentForm.js
@@ -2,21 +2,27 @@ import React, { useCallback } from "react";
 import { Form, Input, Button } from "antd";
 import useInput from "../hooks/useInput";
 import PropTypes from "prop-types";
+import { useSelector } from "react-redux";
 
 const CommentForm = ({ post }) => {
-  const [commentText, setCommentText] = useInput("");
+  const id = useSelector((state) => state.user.me?.id);
+  const [commentText, onChangeCommentText] = useInput("");
   const onSubmitComment = useCallback(() => {
     console.log(post.id, commentText);
   }, [commentText]);
   return (
     <Form onFinish={onSubmitComment}>
-      <Form.Item>
-        <Input.Textarea
+      <Form.Item style={{ position: "relative", margin: 0 }}>
+        <Input.TextArea
           value={commentText}
           onChange={onChangeCommentText}
           rows={4}
         />
-        <Button type="primary" htmlType="submit">
+        <Button
+          style={{ position: "absolute", right: 0, bottom: -40 }}
+          type="primary"
+          htmlType="submit"
+        >
           댓글달기
         </Button>
       </Form.Item>
