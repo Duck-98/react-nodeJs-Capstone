f1005576a2967476df94633bc4c422bf6b1fbe05
duck <godqhr2256@gmail.com>
2022-04-06 23:26:37 +0900

    [frontend] redux-saga ~ing

--- frontend/store/configureStore.js
+++ frontend/store/configureStore.js
@@ -2,9 +2,18 @@ import { createWrapper } from "next-redux-wrapper";
 import { applyMiddleware, compose, createStore } from "redux";
 import reducer from "../reducers";
 import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
+
+const loggerMiddleware =
+  ({ dispatch, getState }) =>
+  (next) =>
+  (action) => {
+    console.log(action);
+    return next(action);
+  };
 
 const configureStore = () => {
-  const middlewares = [];
+  const sagamiddlewares = createSagaMiddleware();
   const enhancer =
     process.env.NODE_ENV === "production"
       ? compose(applyMiddleware(...middlewares)) // 배포용
